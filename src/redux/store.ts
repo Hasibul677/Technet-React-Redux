d604804415eedb17ed74b72b89d6dbfdcce51c2f
Hasibul677 <hhasibul677@gmail.com>
2024-01-01 11:32:41 +0600

    before cart

---
+++ src/redux/store.ts
@@ -0,0 +1,14 @@
+import {configureStore} from "@reduxjs/toolkit"
+import cartSliceReducer from "./features/cart/cartSlice";
+
+
+const store = configureStore({
+    reducer:{
+        cart: cartSliceReducer
+    }
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch; 
+export default store;
+
